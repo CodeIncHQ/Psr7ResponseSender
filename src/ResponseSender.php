90a45782352694308864ebd09de12427224f46c7
Joan Fabrégat <joan@codeinc.fr>
2018-06-12 12:14:25 +0200

    now supports GZ compression of the response's body

--- src/ResponseSender.php
+++ src/ResponseSender.php
@@ -23,27 +23,67 @@ declare(strict_types = 1);
 namespace CodeInc\Psr7ResponseSender;
 use Psr\Http\Message\ResponseInterface;
 
+
 /**
  * Class ResponseSender
  *
  * @package CodeInc\Psr7ResponseSender
- * @author Joan Fabrégat <joan@codeinc.fr>
+ * @author  Joan Fabrégat <joan@codeinc.fr>
  */
 class ResponseSender implements ResponseSenderInterface
 {
     /**
      * @var bool
      */
-    private $removeNativeHttpHeaders;
+    private $removePhpHeaders;
+
+    /**
+     * @var
+     */
+    private $enableGzCompression;
 
     /**
      * ResponseSender constructor.
      *
-     * @param bool $removeNativeHttpHeaders
+     * @param bool $removePhpHeaders
+     * @param bool $enableGzCompression
      */
-    public function __construct(bool $removeNativeHttpHeaders = true)
+    public function __construct(bool $removePhpHeaders = true, bool $enableGzCompression = false)
     {
-        $this->removeNativeHttpHeaders = $removeNativeHttpHeaders;
+        $this->removePhpHeaders = $removePhpHeaders;
+        $this->enableGzCompression = $enableGzCompression;
+    }
+
+    /**
+     * Enables the GZ compression of the body.
+     */
+    public function enableGzCompression():void
+    {
+        $this->enableGzCompression = true;
+    }
+
+    /**
+     * Disables the GZ compression of the body.
+     */
+    public function disableGzCompression():void
+    {
+        $this->enableGzCompression = false;
+    }
+
+    /**
+     * Removes all the native PHP HTTP headers. Only sends the headers included in the PSR-7 response object.
+     */
+    public function removePhpHeaders():void
+    {
+        $this->removePhpHeaders = true;
+    }
+
+    /**
+     * Sends all the native PHP HTTP headers and headers included in the PSR-7 response object.
+     */
+    public function sendPhpHeaders():void
+    {
+        $this->removePhpHeaders = false;
     }
 
     /**
@@ -52,37 +92,32 @@ class ResponseSender implements ResponseSenderInterface
      */
     public function send(ResponseInterface $response):void
     {
-        // checking
-        if (headers_sent()) {
-            throw new ResponsSenderException("A response has already been sent to the web browser",
-                $this);
-        }
-
-        // sending
-        if ($this->removeNativeHttpHeaders) {
-            $this->remoteNativeHttpHeaders();
-        }
-        $this->sendResponseHttpHeaders($response);
-        $this->sendResponseBody($response);
-    }
-
-    /**
-     * Removes the native PHP HTTP headers.
-     */
-    private function remoteNativeHttpHeaders():void
-    {
-        foreach (headers_list() as $header) {
-            header_remove(explode(":", $header)[0]);
-        }
+        $this->sendHttpHeaders($response);
+        $this->sendBody($response);
     }
 
     /**
      * Sends the response HTTP headers.
      *
      * @param ResponseInterface $response
+     * @throws ResponsSenderException
      */
-    private function sendResponseHttpHeaders(ResponseInterface $response):void
+    public function sendHttpHeaders(ResponseInterface $response):void
     {
+        // checking
+        if (headers_sent()) {
+            throw new ResponsSenderException("A response has already been sent to the web browser",
+                $this);
+        }
+
+        // removing PHP native HTTP headers (if enabled)
+        if ($this->removePhpHeaders) {
+            foreach (headers_list() as $header) {
+                header_remove(explode(":", $header)[0]);
+            }
+        }
+
+        // sending response HTTP headers
         header("HTTP/{$response->getProtocolVersion()} {$response->getStatusCode()} "
             ."{$response->getReasonPhrase()}", true);
         foreach ($response->getHeaders() as $header => $values) {
@@ -97,8 +132,11 @@ class ResponseSender implements ResponseSenderInterface
      *
      * @param ResponseInterface $response
      */
-    private function sendResponseBody(ResponseInterface $response):void
+    public function sendBody(ResponseInterface $response):void
     {
+        if ($this->enableGzCompression) {
+            ob_start('ob_gzhandler');
+        }
         if (($resource = $response->getBody()->detach()) !== null) {
             fpassthru($resource);
             fclose($resource);
